9b7ece1e26992ec26cb14238d030c68398e22787
mys1024 <mys.1024@outlook.com>
2022-10-01 17:22:49 +0800

    feat: vite-plugin-inspect

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 import Unocss from 'unocss/vite'
 import Wat from 'vite-plugin-wat'
+import Inspect from 'vite-plugin-inspect'
 
 export default defineConfig({
   plugins: [
@@ -12,6 +13,8 @@ export default defineConfig({
     Unocss(),
     // https://github.com/mys1024/vite-plugin-wat
     Wat(),
+    // https://github.com/antfu/vite-plugin-inspect
+    Inspect(),
   ],
   // https://github.com/vitest-dev/vitest
   test: {
